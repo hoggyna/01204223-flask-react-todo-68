3e2a93eab49db5ab359b092f74090c4fa0b9e64f
hoggyna <hoggynainwza@gmail.com>
2026-02-20 21:17:44 +0700

    freez

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import TodoItem, Comment, db                             # import จาก models
+from models import TodoItem, Comment, User, db                      # import จาก models
+import click
+from werkzeug.security import generate_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
@@ -13,7 +17,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 db.init_app(app)                                                     # แก้จาก db = SQLAlchemy(app, model_class=Base)
 migrate = Migrate(app, db)
 
+app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+jwt = JWTManager(app)
 
+@app.cli.command("create-user")
+@click.argument("username")
+@click.argument("full_name")
+@click.argument("password")
+@app.route('/api/login/', methods=['POST'])
+
+
+def login():
+    data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({'error': 'Username and password are required'}), 400
+
+    user = User.query.filter_by(username=data['username']).first()
+    if not user or not user.check_password(data['password']):
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+    # ***** สร้าง token แล้วคืนค่ากลับไป
+    access_token = create_access_token(identity=user.username)
+    return jsonify(access_token=access_token)
+
+    return jsonify({'message': 'Login successful'})
+def create_user(username, full_name, password):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        click.echo("User already exists.")
+        return
+    user = User(username=username, full_name=full_name)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    click.echo(f"User {username} created successfully.")
 @app.route('/api/todos/', methods=['GET'])
 def get_todos():
     todos = TodoItem.query.all()
