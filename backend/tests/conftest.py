a48d40ccb235b73e4f7dc58a1bfa7594ce0ac573
hoggyna <hoggynainwza@gmail.com>
2026-02-27 15:00:54 +0700

    change import

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
     
+    
 from main import app as flask_app
 from models import db
 import pytest
