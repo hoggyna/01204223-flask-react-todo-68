e9051293dc11538022ecb55176c37308f5f0bd07
hoggyna <hoggynainwza@gmail.com>
2026-02-27 14:53:27 +0700

    fix flask testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,14 +1,16 @@
 import sys
-import pytest
 from pathlib import Path
-from main import app as flask_app
-from models import db
 
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
+    
+from main import app as flask_app
+from models import db
+import pytest
+
 
 @pytest.fixture
 def app():
